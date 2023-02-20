26ee1d54800346bae1032d6e7c1feab38cf5c497
KareemEH <kareemhajjar@hotmail.com>
2023-02-20 13:01:11 -0500

    Created classes and basic MVC file structure.

--- src/main/java/main/BookStoreApp.java
+++ src/main/java/main/BookStoreApp.java
@@ -3,12 +3,16 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Hello world!
+ *
+ */
 @SpringBootApplication
-public class App 
-{
+public class BookStoreApp {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(BookStoreApp.class, args);
     }
 }
