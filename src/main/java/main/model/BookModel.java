b32b5568d733da96e4d95fbed535f51b4bcbaa0b
KareemEH <kareemhajjar@hotmail.com>
2023-02-22 14:13:10 -0500

    - Implemented BookModel and BookRepository. - Removed BookStore and BookStoreRepository. - Added lombok dependency to pom.xml.

--- src/main/java/main/model/BookModel.java
+++ src/main/java/main/model/BookModel.java
@@ -1,4 +1,27 @@
 package main.model;
 
-public class BookModel {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String description;
+    private String author;
+    private String publisher;
 }
