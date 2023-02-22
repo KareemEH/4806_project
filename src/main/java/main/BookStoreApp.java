a196dcaeb56cc4fe3e5249f22c44ae1b83991dbe
b <b>
2023-02-22 11:28:33 -0500

    basic home page handler ready for implementation

--- src/main/java/main/BookStoreApp.java
+++ src/main/java/main/BookStoreApp.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class BookStoreApp {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Starting BookStoreApp" );
 		SpringApplication.run(BookStoreApp.class, args);
     }
 }
