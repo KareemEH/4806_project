4d0eb6df46121f74b2cda06a70438b9566c18ca5
Mohamed Abdalla <78317393+Mohman556@users.noreply.github.com>
2023-02-22 13:29:12 -0500

    Added basic GUIController functionality. Code is commented out for now to avoid Errors. Will be uncommented once other features are added.

--- src/main/java/main/controller/GUIController.java
+++ src/main/java/main/controller/GUIController.java
@@ -1,13 +1,112 @@
-package main.controller;
+// package main.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class GUIController {
+// import main.model.BookStoreModel;
+// import main.model.ShoppingCartModel;
+// import main.model.UserModel;
+// import main.repository.BookStoreRepository;
+// import main.repository.ShoppingCartRepository;
+// import main.repository.UserRepository;
 
-    @GetMapping("/")
-    public String homePage(){
-        return "index";
-    }
-}
+// @Controller
+// public class GUIController {
+
+//     @Autowired
+//     private final BookStoreRepository storeRepo;
+//     private final ShoppingCartRepository cartRepo;
+//     private final UserRepository userRepo;
+
+
+//     public GUIController(BookStoreRepository storeRepo, ShoppingCartRepository cartRepo, UserRepository userRepo){
+//         this.storeRepo = storeRepo;
+//         this.cartRepo = cartRepo;
+//         this.userRepo = userRepo;
+//     }
+
+//     /**
+//      * Home Page.
+//      * This page will display the Bookstore, and the books it contains.
+//      * 
+//      * @param model
+//      * @return index.html
+//      */
+//     @GetMapping("/home")
+//     public String homePage(Model model){
+//         BookStoreModel bookStore = storeRepo.findByName("Store1");
+//         if (bookStore == null){
+//             bookStore = new BookStoreModel("Store1");
+//             storeRepo.save(bookStore);
+//         }
+//         model.addAttribute("BookStoreModel", bookStore);
+        
+//         //Add list of books?
+
+
+//         return "index";
+//     }
+
+//     /**
+//      * Cart Page.
+//      * This page will display the Cart, and the books it contains.
+//      * 
+//      * @param model
+//      * @return index.html
+//      */
+//     @GetMapping("/cart")
+//     public String cartPage(Model model){
+//         ShoppingCartModel cart = cartRepo.findByName("Cart1");
+//         if (cart == null){
+//             cart = new ShoppingCartModel("Cart1");
+//             cartRepo.save(cart);
+//         }
+//         model.addAttribute("ShoppingCartModel", cart);
+        
+//         //Add list of orders?
+
+
+//         return "index";
+//     }
+
+//     /**
+//      * User Page.
+//      * This page will display the User's Information.
+//      * 
+//      * @param model
+//      * @return index.html
+//      */
+//     @GetMapping("/user")
+//     public String userPage(Model model){
+//         UserModel user = userRepo.findByName("User1");
+//         if (user == null){
+//             user = new UserModel("User1");
+//             userRepo.save(user);
+//         }
+//         model.addAttribute("UserModel", user);
+        
+//         //Add list of user information (UserName, Addresses...)?
+
+
+//         return "index";
+//     }
+
+//     /**
+//      * User Page.
+//      * This page will display the User's Information.
+//      * 
+//      * @param model
+//      * @return index.html
+//      */
+//     @GetMapping("/admin")
+//     public String adminPage(Model model){
+
+        
+        
+
+
+//         return "index";
+//     }
+// }
