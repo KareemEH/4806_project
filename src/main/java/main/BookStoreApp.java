41884a8fb777e7779fb09b2229524c716689e5b1
KareemEH <kareemhajjar@hotmail.com>
2023-03-05 16:49:44 -0500

    - Changed all repositories to use JPARepository instead of CrudRepository due to some errors. - Also changed all models to store ids as Long and not Integer.

--- src/main/java/main/BookStoreApp.java
+++ src/main/java/main/BookStoreApp.java
@@ -3,7 +3,6 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class BookStoreApp {
     public static void main( String[] args )
--- src/main/java/main/repository/UserRepository.java
+++ src/main/java/main/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package main.repository;
 import main.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserModel, Integer> {
-    UserModel findById(Long id);
-    UserModel findByName(String username);
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByUsername(String username);
 }
