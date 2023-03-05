03fa093e29ffb1552e2762e7a32878e4e6762999
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2023-03-05 15:50:21 -0500

    Added ShoppingCartModel and basic ShoppingCartRepository

--- src/main/java/main/repository/ShoppingCartRepository.java
+++ src/main/java/main/repository/ShoppingCartRepository.java
@@ -1,4 +1,9 @@
 package main.repository;
 
-public interface ShoppingCartRepository {
+import main.model.ShoppingCartModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingCartRepository extends CrudRepository<ShoppingCartModel, Integer> {
 }
