b66d242d8aa4621f16232fd81b25777aeb0ce470
b <b>
2023-03-07 22:59:21 -0500

    set up jasmine

--- src/main/resources/static/javascript/common.js
+++ src/main/resources/static/javascript/common.js
@@ -12,3 +12,23 @@ function goToLogin(bookName){
 function goToRegister(bookName){
     document.location.href = `/register`; 
 }
+
+function goToBook(id){
+    document.location.href = `/book?id=${id}`; 
+}
+
+function addBookButton(element){
+    const button = document.createElement('button');
+    button.classList.add("book-button");
+    button.setAttribute("onclick", `goToBook(${element.id})`);
+
+    const img = document.createElement('img');
+    img.classList.add("book-button-img");
+    img.setAttribute("src", `images/${element.title.toLowerCase().replace(" ", "_")}.jpg`);
+    img.setAttribute("alt", "Book picture not found");
+
+    button.appendChild(img);
+
+    const homepage_books = document.getElementById("homepage-books");
+    homepage_books.appendChild(button);
+}
--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -1,22 +1,3 @@
-function goToBook(id){
-    document.location.href = `/book?id=${id}`; 
-}
-
-function addBookButton(element){
-    const button = document.createElement('button');
-    button.classList.add("book-button");
-    button.setAttribute("onclick", `goToBook(${element.id})`);
-
-    const img = document.createElement('img');
-    img.classList.add("book-button-img");
-    img.setAttribute("src", `images/${element.title.toLowerCase().replace(" ", "_")}.jpg`);
-    img.setAttribute("alt", "Book picture not found");
-
-    button.appendChild(img);
-
-    const homepage_books = document.getElementById("homepage-books");
-    homepage_books.appendChild(button);
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch("/frontPageBooks").
