e935681585d2d03b32dcf640292fb31fc80d0ebe
b <b>
2023-03-07 23:11:52 -0500

    basic test for username/password validation done

--- src/main/resources/static/javascript/common.js
+++ src/main/resources/static/javascript/common.js
@@ -32,3 +32,111 @@ function addBookButton(element){
     const homepage_books = document.getElementById("homepage-books");
     homepage_books.appendChild(button);
 }
+
+function getUsername(){
+    return document.getElementById("username-field").value;
+}
+
+function getPassword(){
+    return document.getElementById("password-field").value;
+}
+
+function validUsername(username){
+    if(username === ""){
+        return false;
+    }
+    
+    return true;
+}
+
+function validPassword(password){
+    if(password === ""){
+        return false;
+    }
+
+    return true;
+}
+
+async function login(username, password){
+    return fetch("/verify_login", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+    })
+}
+
+function attemptLogin(){
+    username = getUsername();
+    password = getPassword();
+
+    if(!validUsername || !validPassword()){
+        // TODO more graceful failiure
+        console.log("login failure (client side)");
+        alert("login failure (client side)");
+
+        return;
+    }
+
+    login(username, password)
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((obj) => {
+        if(obj.success){
+            alert("Successful login!");
+            document.location.href = "/";
+        }
+        else{
+            console.log("login failure (server side)");
+            alert("Login failure (server side)");
+        }
+    });
+}
+
+async function registerNewUser(username, password){
+    return fetch("/new_user", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+    })
+}
+
+function attemptRegistration(){
+    username = getUsername();
+    password = getPassword();
+
+    if(!validUsername || !validPassword()){
+        // TODO more graceful failiure
+        console.log("registration error (client side)");
+        alert("registration error (client side)");
+
+        return;
+    }
+
+    registerNewUser(username, password)
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((obj) => {
+        if(obj.success){
+            alert("Successful registration!");
+            document.location.href = "/";
+        }
+        else{
+            console.log("registration error (server side)");
+            alert("registration error (server side)");
+        }
+    });
+}
