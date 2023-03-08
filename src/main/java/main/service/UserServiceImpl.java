ebcc7e7d08039fde05d1509a62e69cdffe3a9a14
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2023-03-08 12:28:19 -0500

    - Fixed error where it would allow the user to login with incorrect password. - Cleaned up verify login method. - Changed createUser method to void in UserService and UserServiceImpl

--- src/main/java/main/service/UserServiceImpl.java
+++ src/main/java/main/service/UserServiceImpl.java
@@ -15,10 +15,9 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public UserModel createUser(String username, String password) {
+    public void createUser(String username, String password) {
         UserModel userModel = new UserModel(username, password);
         userRepo.save(userModel);
-        return userModel;
     }
 
     @Override
