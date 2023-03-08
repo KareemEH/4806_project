ed55b45cb24baec690191abb55abc1ed79e0ab8a
Brandon <Brandon>
2023-03-08 12:38:59 -0500

    added more front end unit tests

--- jasmine/spec/testSpec.js
+++ jasmine/spec/testSpec.js
@@ -11,4 +11,60 @@ describe('frontEndTests', function() {
     expect(validUsername("testUser&098")).toBeTrue();
     expect(validUsername("")).toBeFalse();
   });
+
+  it('Test getting Usernames', function() {
+    expect(getUsername()).toEqual("testUsername");
+  });
+
+  it('Test getting Password', function() {
+    expect(getPassword()).toEqual("testPassword");
+  });
+
+  it('Test that fetch is called when logging in', function() {
+      fetch = jasmine.createSpy();
+      login("testUser", "testPass");
+      expect(fetch).toHaveBeenCalledWith("/verify_login", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({username: "testUser", password: "testPass"})
+      });
+
+      fetch = jasmine.createSpy();
+      login("", "");
+      expect(fetch).toHaveBeenCalledWith("/verify_login", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({username: "", password: ""})
+      });
+  });
+
+  it('Test that fetch is called when registering', function() {
+    fetch = jasmine.createSpy();
+    registerNewUser("testUser", "testPass");
+    expect(fetch).toHaveBeenCalledWith("/new_user", {
+      method: "POST",
+      headers: {
+          'Content-Type': "application/json",
+          'Accept': "application/json",
+      },
+      body: JSON.stringify({username: "testUser", password: "testPass"})
+    });
+
+    fetch = jasmine.createSpy();
+    registerNewUser("", "");
+    expect(fetch).toHaveBeenCalledWith("/new_user", {
+      method: "POST",
+      headers: {
+          'Content-Type': "application/json",
+          'Accept': "application/json",
+      },
+      body: JSON.stringify({username: "", password: ""})
+    });
+  });
 });
