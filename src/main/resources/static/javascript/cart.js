07c651a74aad0d4b36a649ff627190f8f52ed2bc
b <b>
2023-03-19 21:27:09 -0400

    some stuff with cart page i think

--- src/main/resources/static/javascript/cart.js
+++ src/main/resources/static/javascript/cart.js
@@ -1,10 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("/frontPageBooks").
-    then((payload) => payload.json()).
-    then((json) => json.forEach(element => {
-        addBookButton(element);        
-    }));
+    // do fetch and call add cartItem()
+    // fetch("/frontPageBooks").
+    // then((payload) => payload.json()).
+    // then((json) => json.forEach(element => {
+    //     addBookButton(element);        
+    // }));
+
+    addCartItem(1, 3);
+    addCartItem(2, 5);
 
     if(sessionStorage.getItem("loggedIn") === 'true'){
         const btns = document.getElementsByClassName('login-reg-btn');
@@ -16,3 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
         log_out_text.innerHTML = 'Logout: ' + sessionStorage.getItem("username");
     }
 })
+
+function addCartItem(bookID, quantity){
+    fetch("/bookByID?id=" + bookID).
+    then((payload) => payload.json()).
+    then((json) => {
+        console.log(json.title + " * " + quantity);
+
+        const p = document.createElement('p');
+        
+        p.innerHTML = json.title + " * " + quantity;
+
+        const cart_items = document.getElementById("cart-items");
+        cart_items.appendChild(p);
+    });
+}
