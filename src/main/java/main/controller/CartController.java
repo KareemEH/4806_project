84edab400a9c10328a3696db838a15262c0bb5f5
Mohamed Abdalla <78317393+Mohman556@users.noreply.github.com>
2023-03-20 13:05:28 -0400

    Implemented Cart Controller Functionality: Adding to Cart, View Cart. Made a tweak to Verify Login, Now returns userid as well. Made a stub for now in Userservice.

--- src/main/java/main/controller/CartController.java
+++ src/main/java/main/controller/CartController.java
@@ -1,34 +1,52 @@
 package main.controller;
 
+import main.model.UserModel;
+import main.repository.UserRepository;
+import main.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import main.model.BookModel;
 import main.repository.BookRepository;
-import org.springframework.ui.Model;
 
+import java.util.HashMap;
+import java.util.Map;
 
-
+@RestController
 public class CartController {
     
-    @Autowired
-    private BookRepository bookRepository;
 
-    @GetMapping("")
-    public String getBooks(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
-        return "books";
+    private BookRepository bookRepository;
+    private final UserService userService;
+    private UserRepository userRepository;
+
+    @Autowired
+    public CartController(UserService userService) {
+        this.userService = userService;
     }
 
 
+    @GetMapping(value="bookByID", produces= MediaType.APPLICATION_JSON_VALUE)
+    public Map<BookModel, Integer> getBooks(@RequestParam("userid") long userid) {
+        UserModel user = userRepository.findById(userid);
+        Map<BookModel, Integer> Books = user.getShoppingCart().getBookQuantityMap();
+
+        return Books;
+    }
+
     /**
-     * Adding a book to database. TO be expanded upon later
-     * @param book
+     * Adding a book to Cart. TO be expanded upon later
+     * @param userid
+     * @param bookid
      * @return
      */
     @PostMapping("")
-    public String addBook(@ModelAttribute BookModel book) {
-        bookRepository.save(book);
-        return "redirect:/books";
+    public String addBook(@RequestParam("userid") long userid, @RequestParam("bookid") long bookid, @RequestParam("quantity") int quantity) {
+        boolean addTo = userService.addtoCart(userid,bookid, quantity);
+        if (addTo){
+            return  "{\"success\": true}";
+        }
+        return  "{\"success\": false}";
     }
 
 }
