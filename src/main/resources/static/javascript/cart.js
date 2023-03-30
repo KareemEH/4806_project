2c69c57674f18dacb677d97ec44a978c9a8490fe
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2023-03-30 18:03:49 -0400

    Added search functionalities to cart and order pages using datatables jQuery plugin.

--- src/main/resources/static/javascript/cart.js
+++ src/main/resources/static/javascript/cart.js
@@ -33,12 +33,16 @@ function getUserCart(){
         .then((payload) => payload.json())
         .then((json) => {
             const table = document.createElement('table');
+            table.setAttribute("id", "cart");
             const header = table.createTHead();
-            const row = header.insertRow(0);
-            const titleHeader = row.insertCell(0);
-            const authorHeader = row.insertCell(1);
-            const priceHeader = row.insertCell(2);
-            const quantityHeader = row.insertCell(3);
+            const body = table.createTBody();
+            const footer = table.createTFoot();
+            const headerRow = header.insertRow(0);
+            const titleHeader = headerRow.insertCell(0);
+            const authorHeader = headerRow.insertCell(1);
+            const priceHeader = headerRow.insertCell(2);
+            const quantityHeader = headerRow.insertCell(3);
+            headerRow.insertCell(4);
             titleHeader.innerHTML = '<b>Title</b>';
             authorHeader.innerHTML = '<b>Author</b>';
             priceHeader.innerHTML = '<b>Price</b>';
@@ -50,7 +54,7 @@ function getUserCart(){
                 const author = bookArray[1];
                 const price = bookArray[2];
                 const quantity = bookArray[3];
-                const row = table.insertRow(-1);
+                const row = body.insertRow(-1);
                 const titleCell = row.insertCell(0);
                 const authorCell = row.insertCell(1);
                 const priceCell = row.insertCell(2);
@@ -71,18 +75,22 @@ function getUserCart(){
             });
 
             // Add row for total price
-            const row1 = table.insertRow(-1);
+            const row1 = footer.insertRow(-1);
             const totalHeader = row1.insertCell(0);
             const totalCell = row1.insertCell(1);
+            // Adding empty cells to keep the table consistent with column numbers
+            row1.insertCell(2).innerHTML;
+            row1.insertCell(3).innerHTML;
+            row1.insertCell(4).innerHTML;
             totalHeader.innerHTML = '<b>Total Price:</b>';
             totalCell.innerHTML = "$" + totalPrice.toFixed(2); // display total with 2 decimal places
 
             const cartItemsDiv = document.getElementById("cart-items");
             cartItemsDiv.innerHTML = ""; // clear the div contents
             cartItemsDiv.appendChild(table); // add the table to the div
+            $('#cart').DataTable();
         });
 }
-
 async function deleteFromCart(user_id, book_id){
     return fetch("/deleteFromCart?userid=" + user_id + " &bookId=" + book_id, {
         method: "DELETE",
